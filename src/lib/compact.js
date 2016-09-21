37dd5dee4e5ac98d1e4c3d3793fdd18ed93b3c38
Sylvan Swierkosz <sswierkosz@gmail.com>
2016-09-21 12:18:04 -0700

    refactored  compact.js

--- src/lib/compact.js
+++ src/lib/compact.js
@@ -1,13 +1,8 @@
-const compact = (arr) => {
+const compact = arr => {
   let trueArr = []
-  if (arr == undefined) {
-    return trueArr
-  }
+  if (arr == undefined) { return trueArr }
   for (let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] === 'string' && arr[i] != '') {
-      trueArr.push(arr[i])
-    }
-    if (arr[i] != false && arr[i] != null && arr[i] != undefined && !isNaN(arr[i])) {
+    if (arr[i]) {
       trueArr.push(arr[i])
     }
   }
