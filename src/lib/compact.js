bbcd1790a93731794329d1767f925e2b69d9f6cc
Sylvan Swierkosz <sswierkosz@gmail.com>
2016-09-21 11:38:51 -0700

    added compact function and test

---
+++ src/lib/compact.js
@@ -0,0 +1,17 @@
+const compact = (arr) => {
+  let trueArr = []
+  // if (typeof arr == 'undefined') {
+  //   return trueArr
+  // }
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'string' && arr[i] != '') {
+      trueArr.push(arr[i])
+    }
+    if (arr[i] != false && arr[i] != null && arr[i] != undefined && !isNaN(arr[i])) {
+      trueArr.push(arr[i])
+    }
+  }
+  return trueArr
+}
+
+export default compact
